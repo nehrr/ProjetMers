bd73964f8bbc102fc696db22f417a93abe7f07b1
nate <iamkapitaan@gmail.com>
2016-11-02 14:16:52 +0100

    Menu + pages base

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  root 'pages#home' 
+  
+  get 'home' => 'pages#home'
+  
+  get 'comm' => 'pages#comm-ecrite'
+  get 'contact' => 'pages#contact'
+  get 'extraits' => 'pages#extraits-vid'
+  get 'info' => 'pages#info'
+  get 'photos' => 'pages#photos'
+  get 'realisations' => 'pages#realisations'
+  get 'references' => 'pages#references'
+  get 'spectacle' => 'pages#spec-vivant'
+  get 'tarifs' => 'pages#tarifs'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
